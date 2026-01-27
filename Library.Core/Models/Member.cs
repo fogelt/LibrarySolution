8272f6946f724d88f1f6b26ca1b059fd1705742f
fogelt <fogeltt@gmail.com>
2026-01-27 23:41:42 +0100

    implemented Member class

---
+++ Library.Core/Models/Member.cs
@@ -0,0 +1,16 @@
+namespace Library.Core.Models;
+
+public class Member(string memberId, string name, string email, DateTime memberSince, List<Book> inventory)
+{
+  string MemberId { get; set; } = memberId;
+  string Name { get; set; } = name;
+  string Email { get; set; } = email;
+  DateTime MemberSince { get; set; } = memberSince;
+  List<Book> Inventory { get; set; } = inventory;
+
+  public string GetInfo()
+  {
+    string InventoryStatus = Inventory.Count == 0 ? "No books on loan" : $"Books on loan: {Inventory}";
+    return $"[{MemberId}] \nName: {Name} \nEmail: {Email} \nJoined on: {MemberSince} \n{InventoryStatus}";
+  }
+}
